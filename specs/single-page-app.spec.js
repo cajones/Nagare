644e2fa97a95bbb52843e210fbfec3deab77110a
Chris Jones <chrisjones0099@hotmail.com>
2012-02-03 17:05:44 +0000

    SPA rendering under test

---
+++ specs/single-page-app.spec.js
@@ -0,0 +1,20 @@
+var sinon = require('sinon');
+
+describe('Boardly - web application', function() {
+	
+	var handler = require('../routes/single-page-app.js').handler,
+		request = {},
+		response = { 
+			render: sinon.spy()
+		};
+
+	describe('when a user gets the index page', function() {
+		
+		handler.getIndex(request, response);
+
+		it('should render the single page app', function() {
+				
+			response.render.calledWith('single-page-app').should.be.ok;
+		});
+	});		
+});
